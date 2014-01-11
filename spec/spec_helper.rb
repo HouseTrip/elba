3a1ec8ebaf313e1bd4b750f049d34ecda8183e89
Thibault Gautriaud <tgautriaud@housetrip.com>
2014-01-11 11:12:02 +0000

    Enable color and format documentation in test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ require 'elba'
 require 'support/mocks'
 
 RSpec.configure do |config|
+  config.color_enabled = true
+  config.formatter     = :documentation
+
   config.before(:each) do
     # Pretend we're running as 'elba'
     $0 = "elba"
